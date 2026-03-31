e04126b4a16e99d91f47c48d86f276f48215a2af
thushini21 <chamaldinupa58@gmail.com>
2026-03-31 07:22:29 +0530

    updated  ecosystem.js file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,19 +2,19 @@ module.exports = {
   apps: [
     {
       name: "config-server",
-      script: "/usr/bin/java",
+      script: "/home/jagathanura2003/.sdkman/candidates/java/current/bin/java",
       args: "-jar /home/jagathanura2003/Capstone-platform/Capstone-config-server/target/Config-Server-1.0.0.jar",
       watch: false
     },
     {
       name: "service-registry",
-      script: "/usr/bin/java",
+      script: "/home/jagathanura2003/.sdkman/candidates/java/current/bin/java",
       args: "-jar /home/jagathanura2003/Capstone-platform/Capstone-service-registry/target/Service-Registry-1.0.0.jar",
       watch: false
     },
     {
       name: "api-gateway",
-      script: "/usr/bin/java",
+      script: "/home/jagathanura2003/.sdkman/candidates/java/current/bin/java",
       args: "-jar /home/jagathanura2003/Capstone-platform/Capstone--api-gateway/target/Api-Gateway-1.0.0.jar",
       watch: false
     }
