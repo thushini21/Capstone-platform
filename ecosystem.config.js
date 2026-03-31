84e5c4c0cb2a0a129e8ffe4e9463fa5739d2b37d
thushini21 <chamaldinupa58@gmail.com>
2026-03-31 05:59:04 +0530

    update ecosystem.js file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,29 @@
+const path = require("path");
+
+const isWin = process.platform === "win32";
+
 module.exports = {
   apps: [
     {
       name: "config-server",
-      script: "C:\\Users\\Thushini Akashi\\.jdks\\ms-25.0.2\\bin\\java.exe",
-      args: "-jar ./Capstone-config-server/target/Config-Server-1.0.0.jar"
+      script: "java",
+      args: `-jar ${isWin ? ".\\Capstone-config-server\\target\\Config-Server-1.0.0.jar" : "./config-server/target/Config-Server-1.0.0.jar"}`,
+      cwd: __dirname,
+      watch: false
     },
     {
       name: "service-registry",
-      script: "C:\\Users\\Thushini Akashi\\.jdks\\ms-25.0.2\\bin\\java.exe",
-      args: "-jar ./Capstone-service-registry/target/Service-Registry-1.0.0.jar"
+      script: "java",
+      args: `-jar ${isWin ? ".\\Capstone-service-registry\\target\\Service-Registry-1.0.0.jar" : "./service-registry/target/Service-Registry-1.0.0.jar"}`,
+      cwd: __dirname,
+      watch: false
     },
     {
       name: "api-gateway",
-      script: "C:\\Users\\Thushini Akashi\\.jdks\\ms-25.0.2\\bin\\java.exe",
-      args: "-jar ./Capstone--api-gateway/target/Api-Gateway-1.0.0.jar"
+      script: "java",
+      args: `-jar ${isWin ? ".\\Capstone--api-gateway\\target\\Api-Gateway-1.0.0.jar" : "./api-gateway/target/Api-Gateway-1.0.0.jar"}`,
+      cwd: __dirname,
+      watch: false
     }
   ]
 };
