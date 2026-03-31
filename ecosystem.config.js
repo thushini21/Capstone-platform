cc6f11154882d4339e6341f66252170df921efae
thushini21 <chamaldinupa58@gmail.com>
2026-03-31 12:57:24 +0530

    eco file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,29 +1,19 @@
-const path = require("path");
-
-const isWin = process.platform === "win32";
-
 module.exports = {
   apps: [
     {
       name: "config-server",
       script: "java",
-      args: `-jar ${isWin ? ".\\Capstone-config-server\\target\\Config-Server-1.0.0.jar" : "./Capstone-config-server/target/Config-Server-1.0.0.jar"}`,
-      cwd: __dirname,
-      watch: false
+      args: "-jar Capstone-config-server/target/Config-Server-1.0.0.jar"
     },
     {
       name: "service-registry",
       script: "java",
-      args: `-jar ${isWin ? ".\\Capstone-service-registry\\target\\Service-Registry-1.0.0.jar" : "./Capstone-service-registry/target/Service-Registry-1.0.0.jar"}`,
-      cwd: __dirname,
-      watch: false
+      args: "-jar Capstone-service-registry/target/Service-Registry-1.0.0.jar"
     },
     {
       name: "api-gateway",
       script: "java",
-      args: `-jar ${isWin ? ".\\Capstone--api-gateway\\target\\Api-Gateway-1.0.0.jar" : "./Capstone--api-gateway/target/Api-Gateway-1.0.0.jar"}`,
-      cwd: __dirname,
-      watch: false
+      args: "-jar Capstone--api-gateway/target/Api-Gateway-1.0.0.jar"
     }
   ]
 };
